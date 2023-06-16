c435a91e06240a398f3065965ecd453819fb1474
HeltonAnjos <heltonanjos0@gmail.com>
2023-06-16 15:47:03 -0300

    adicionado novos códigos js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,17 +1,19 @@
 class CalcController {
 
-    constructor(){
+    constructor() {
 
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.iniButtonsEvents();
 
     }
 
-    initialize(){
+    initialize() {
 
         this.setDisplayDateTime();
 
@@ -21,6 +23,153 @@ class CalcController {
             
         }, 1000);
 
+    }
+
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' '),forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        });
+
+    }
+
+    clearAll() {
+
+        this._operation = [];
+
+    }
+
+    clearEntry() {
+
+        this._operation.pop();
+        
+    }
+
+    getLastOperation() {
+ 
+        return this._operation[this._operation.length-1];
+
+    }
+
+    isOperator(value) {
+
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
+
+    }
+
+    addOperation(value) {
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+
+                this._operation[this._operation.length-1] = value;
+
+            } else {
+
+            }
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this._operation.push(newValue);
+        }
+
+        this._operation.push(value)
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+
+            break;
+
+            case 'subtracao':
+
+            break;
+
+            case 'multiplicacao':
+
+            break;
+
+            case 'divisao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+
+                break;
+
+            case 'ponto':
+                
+                break;
+            
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+
+    }
+
+    iniButtonsEvents(){
+
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, 'click drag', e => {
+
+               let textBtn = btn.className.baseVal.replace("btn-","");
+
+               this.execBtn(textBtn);
+
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.computedStyleMap.cursos = "pointer";
+
+            });
+
+        });
+    
     }
 
     setDisplayDateTime(){
@@ -34,35 +183,35 @@ class CalcController {
 
     }
 
-    get displayTime(){
+    get displayTime() {
         return this._timeEl.innerHTML;
     }
 
-    set displayTime(value){
+    set displayTime(value) {
         return this._timeEl.innerHTML = value;
     }
 
-    get displayDate(){
+    get displayDate() {
         return this._dateEl.innerHTML;
     }
 
-    set displayDate(value){
+    set displayDate(value) {
         return this._dateEl.innerHTML = value;
     }
 
-    get displayCalc(){
+    get displayCalc() {
         return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(value){
+    set displayCalc(value) {
         this._displayCalcEl.innerHTML = value;
     }
 
-    get currentDate(){
+    get currentDate() {
         return new Date();
     }
 
-    set currentDate(value){
+    set currentDate(value) {
         this._currentDate = value;
     }
 }
